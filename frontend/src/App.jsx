58d29c6b5698dc416fed79f31cf10a87c3c6e9a5
Shambhavi Pandey <pandeyshambhavi2609@gmail.com>
2025-01-23 01:01:14 +0530

    sahi ho gya

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,28 +1,33 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Sidebar from "./components/Sidebar";
-import LawStudentPage from "./pages/LawStudentPage";
-import LawStudentDashboard from "./pages/LawStudentDashboard";
-import ResearchPapersPage from "./pages/ResearchPapersPage";
-import InternshipPage from "./pages/InternshipPage";
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Lawyers from "./pages/Lawyers";
+import Login from "./pages/Login";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Appointment from "./pages/Appointment";
+import MyAppointments from "./pages/MyAppointments";
+import MyProfile from "./pages/MyProfile";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
-    <Router>
-      <Header />
-      <div className="app__layout">
-        <Sidebar />
-        <main className="app__content">
-          <Routes>
-            <Route path="/" element={<LawStudentPage />} />
-            <Route path="/student-dashboard" element={<LawStudentDashboard />} />
-            <Route path="/research-papers" element={<ResearchPapersPage />} />
-            <Route path="/internships" element={<InternshipPage />} />
-          </Routes>
-        </main>
-      </div>
-    </Router>
+    <div className="mx-4 sm:mx-[10%]">
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/lawyers" element={<Lawyers />} />
+        <Route path="/lawyers/:speciality" element={<Lawyers />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/appointment/:lawyerId" element={<Appointment />} />
+        <Route path="/my-appointments" element={<MyAppointments />} />
+        <Route path="/my-profile" element={<MyProfile />} />
+      </Routes>
+      <Footer />
+    </div>
   );
 };
 
--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "./App";
+import App from "./App.jsx";
 import "./index.css";
+import { BrowserRouter } from "react-router-dom";
+import AppContextProvider from "./context/AppContext.jsx";
 
-ReactDOM.createRoot(document.getElementById("lawstudent-root")).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <BrowserRouter>
+    <AppContextProvider>
+      <App />
+    </AppContextProvider>
+  </BrowserRouter>
 );
