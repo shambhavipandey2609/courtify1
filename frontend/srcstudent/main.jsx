3869fc0b9bf04cf014f6616b489a984b269cb493
Shambhavi Pandey <pandeyshambhavi2609@gmail.com>
2025-01-23 00:09:57 +0530

    student

--- frontend/srcstudent/main.jsx
+++ frontend/srcstudent/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./styles/index.css";
+<script type="module" src="/srcstudent/main.jsx"></script>
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
